fd15059a43a3cd6cb5d442d782dc4d6234eea8ec
Najeeb Anka <najeeb_anka@hotmail.com>
2025-08-28 17:50:23 +0400

    updated contact us page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // script.js
 document.addEventListener('DOMContentLoaded', function(){
+
   // Nav toggle
   document.querySelectorAll('.nav-toggle').forEach(function(btn){
     btn.addEventListener('click', function(){
@@ -19,26 +20,34 @@ document.addEventListener('DOMContentLoaded', function(){
     el.style.transformOrigin = 'center';
   });
 
-});
+  // FAB
+  const fabToggle = document.getElementById('fabToggle');
+  const fabMenu = document.getElementById('fabMenu');
+  let isOpen = false;
 
+  fabToggle.addEventListener('click', () => {
+      isOpen = !isOpen;
+      if (isOpen) {
+          fabMenu.classList.add('show');
+          fabToggle.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+          </svg>`;
+      } else {
+          fabMenu.classList.remove('show');
+          fabToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+          </svg>`;
 
-// FAB
-const fabToggle = document.getElementById('fabToggle');
-const fabMenu = document.getElementById('fabMenu');
-let isOpen = false;
+      }
+  });
 
-fabToggle.addEventListener('click', () => {
-    isOpen = !isOpen;
-    if (isOpen) {
-        fabMenu.classList.add('show');
-        fabToggle.innerHTML =  `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-        </svg>`;
-    } else {
-        fabMenu.classList.remove('show');
-        fabToggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
-        </svg>`;
+  // FAQ
+  const faqs = document.querySelectorAll(".faq");
 
-    }
+  faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+      faq.classList.toggle("active");
+    });
+  });
+
 });
